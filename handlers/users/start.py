5445cc2b494a5109e4801fb18c5d1ac990a23f61
SolijonovAsadbek <iamsolijonovasadbek@gmail.com>
2024-02-05 13:49:30 +0500

    5-fevral 13:49

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -9,9 +9,16 @@ from loader import dp
 async def bot_start(message: types.Message):
     text = (f"Assalomu aleykum <b>{message.from_user.full_name}!</b>\n\n"
             f"<b>O'yin Qoidasi:</b>\n\n"
-            f"<b>O'yin davomida Sizda 3 ta urinish bor</b>. Siz <b>sovg'a</b> "
+            f"<b>O'yin davomida Sizda 3 ta urinish bor</b>.\n\n<b>1-Qoida:</b> Siz <b>sovg'a tanlash</b> "
             f"yoki <b>davom etish</b> tugmasini tanlashingiz mumkin va ohirgi urinishda "
             f"chiqan sovg'a sizga taqdim e'tilgan sov'ga hissoblanadi. "
-            f"Agar xohlasangiz o'yinni <b> birinchi</b> yoki <b>ikkinchi</b> urinishda ham "
-            f"to'xtatishingiz va chiqqan sovg'angizni tanlashingiz mumkin.")
-    await message.answer(text, reply_markup=Buttons.start_game_ready())
+            f"\n\n<b>2-Qoida:</b> Agar xohlasangiz o'yinni <b> birinchi</b> yoki <b>ikkinchi</b> urinishda ham "
+            f"to'xtatishingiz va chiqqan sovg'angizni tanlashingiz mumkin. "
+            f"\n\n<b>3-Qoida:</b> Qo'lga kiritilgan sovg'a faqat shu kunni o'zida "
+            f"<b>MACBOOK</b> xaridi uchun qo'shib beriladi.")
+
+    url = 'media/image/logo/logo.jpg'
+    with open(url, 'rb') as img:
+        await message.answer_photo(img, caption=f"{text}", reply_markup=Buttons.start_game_ready())
+
+    # await message.answer(text, reply_markup=Buttons.start_game_ready())
